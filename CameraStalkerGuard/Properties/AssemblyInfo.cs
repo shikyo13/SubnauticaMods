21dff2f59b770a7614f4cfd7fa62c80ef67db79e
ZeroTheAbsolute <shikyo13@gmail.com>
2026-02-19 12:53:58 -0600

    feat(CameraStalkerGuard): add project scaffolding

---
+++ CameraStalkerGuard/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CameraStalkerGuard")]
+[assembly: AssemblyDescription("Prevents stalkers from stealing scanner room cameras")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CameraStalkerGuard")]
+[assembly: AssemblyCopyright("Adam 2026")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
