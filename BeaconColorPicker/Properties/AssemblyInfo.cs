64b3fa9cd36419fb9fc2bca3e8bed51a64258a5a
ZeroTheAbsolute <shikyo13@gmail.com>
2026-02-19 15:01:23 -0600

    feat(BeaconColorPicker): add project scaffolding

---
+++ BeaconColorPicker/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BeaconColorPicker")]
+[assembly: AssemblyDescription("HSV color picker for beacon and signal pings")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BeaconColorPicker")]
+[assembly: AssemblyCopyright("Adam 2026")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
